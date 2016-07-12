46bb854012954827915b964fdbf359093324dc93
William Beck <echarger1@gmail.com>
2016-07-12 17:14:54 -0400

    Create Selectbasics.sql

---
+++ Selectbasics.sql
@@ -0,0 +1,10 @@
+-- Challenge 1
+ SELECT population FROM world
+  WHERE name = 'Germany'
+  SELECT name, population FROM world
+  WHERE name IN ('Ireland', 'Iceland', 'Denmark');
+  SELECT name, area FROM world
+  WHERE area BETWEEN 200000  AND 250000 
+-- Challenge 2
+-- Challenge 3
+-- Challenge 4
