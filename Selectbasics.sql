7e4f6425f983a4b42f0ef73af057010094a1a4da
William Beck <echarger1@gmail.com>
2016-07-12 19:41:12 -0400

    Update Selectbasics.sql

--- Selectbasics.sql
+++ Selectbasics.sql
@@ -58,3 +58,35 @@ SELECT name, continent, population FROM world
   WHERE gdp > 1000000000000
   
 -- Challenge 4
+
+SELECT yr, subject, winner
+  FROM nobel
+  WHERE yr = 1950;
+  SELECT winner
+  FROM nobel
+  WHERE yr = 1962
+  AND subject = 'Literature';
+  SELECT yr, subject
+  FROM nobel
+  WHERE winner = 'Albert Einstein';
+  SELECT winner
+  FROM nobel
+  WHERE subject = 'PEACE' AND yr >= 2000;
+  SELECT yr, subject, winner
+  FROM nobel
+  WHERE yr BETWEEN 1980 AND 1989 AND subject = 'Literature';
+  SELECT *
+  FROM nobel
+  WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson','Jimmy Carter');
+  SELECT winner
+  FROM nobel
+  WHERE winner LIKE 'John%';
+  SELECT *
+  FROM nobel
+  WHERE subject = 'Physics' AND yr = '1980' OR subject ='Chemistry' AND yr = '1984';
+  SELECT *
+  FROM nobel
+  WHERE yr = '1980' AND subject NOT IN ('Chemistry', 'Medicine');
+  SELECT *
+  FROM nobel
+  WHERE subject = 'Medicine' AND yr < '1910' OR subject = 'Literature' AND yr >= '2004'
